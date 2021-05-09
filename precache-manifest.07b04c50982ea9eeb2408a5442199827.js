9511079adb1b4f5a4c661ffb3cdacf727bc00e91
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-05-09 12:22:56 +0530

    Updates

---
+++ precache-manifest.07b04c50982ea9eeb2408a5442199827.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d532c39a740023c9ae9dc7f76f2b03f",
+    "url": "/CloudWays-Pricing/index.html"
+  },
+  {
+    "revision": "b934222c4d1a654783de",
+    "url": "/CloudWays-Pricing/static/js/2.783aeba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CloudWays-Pricing/static/js/2.783aeba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a566e6fff824df3b7220",
+    "url": "/CloudWays-Pricing/static/js/main.8d0fbf9a.chunk.js"
+  },
+  {
+    "revision": "f7c88029a441628958eb",
+    "url": "/CloudWays-Pricing/static/js/runtime-main.76de27fc.js"
+  },
+  {
+    "revision": "f20a50bb79803cf7dea48d545efd7834",
+    "url": "/CloudWays-Pricing/static/media/bookmark.f20a50bb.svg"
+  },
+  {
+    "revision": "7e246b49875e3f5af2a3233f8dcbf822",
+    "url": "/CloudWays-Pricing/static/media/chat.7e246b49.svg"
+  },
+  {
+    "revision": "7b819aec9ee82e43637f25da1fa75830",
+    "url": "/CloudWays-Pricing/static/media/chevron-small-right.7b819aec.svg"
+  },
+  {
+    "revision": "d0491a51110c0680ad5a245f009f7e74",
+    "url": "/CloudWays-Pricing/static/media/emoji-happy.d0491a51.svg"
+  },
+  {
+    "revision": "9b729c121fdae210894ffa9fabaec6cf",
+    "url": "/CloudWays-Pricing/static/media/emoji-neutral.9b729c12.svg"
+  },
+  {
+    "revision": "ec1deb01ac753e7981515ee21b50ae45",
+    "url": "/CloudWays-Pricing/static/media/emoji-sad.ec1deb01.svg"
+  },
+  {
+    "revision": "a68266a33ff4a9b2129e46d0184fa038",
+    "url": "/CloudWays-Pricing/static/media/location-pin.a68266a3.svg"
+  },
+  {
+    "revision": "35ee1bf45f23a4680001b609f1272edf",
+    "url": "/CloudWays-Pricing/static/media/logo1.35ee1bf4.png"
+  },
+  {
+    "revision": "7d0871a2719dfaa89abcdaf2a6d50c74",
+    "url": "/CloudWays-Pricing/static/media/star.7d0871a2.svg"
+  }
+]);
